8f8585e5eb921c4bae72702802b22541afce5a6c
Esteban Cortes <hola@ecortes.cl>
2022-02-27 13:56:37 -0300

    feat; primer montaje

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h1>EXAMPLE EXPORT PDF</h1>
+            Demo de como exportar un pdf con nuxt
+        </div>
+        <div class="col-6">
+
+    <CotizarPDF></CotizarPDF>
+        </div>
+
+    </div>
+
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'IndexPage',
+
+}
+</script>
---
+++ plugins/vue-html2pdf.js
@@ -0,0 +1,4 @@
+// plugins/vue-html2pdf.js
+import Vue from 'vue'
+import VueHtml2pdf from 'vue-html2pdf'
+Vue.use(VueHtml2pdf)
